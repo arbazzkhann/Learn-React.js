8dd2ede22d10ab3201a740c63c3676a057cb6103
Arbaz Khan <arbazkha78az@gmail.com>
2023-11-22 20:54:07 +0530

    creating custom react project

---
+++ customReact/customreact.js
@@ -0,0 +1,9 @@
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com/'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.getElementById('root')
