a7b90645642742058e9ff07766231cb53f803dff
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-01-09 18:47:52 +0530

    cutom changes

--- 01_vite-project-react/src/main.jsx
+++ 01_vite-project-react/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <h1>Custom Reactttt!</h1>
+  )
+}
+
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://www.google.com', target: '_black'},
+  'click me to visit google'
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    reactElement
 )
