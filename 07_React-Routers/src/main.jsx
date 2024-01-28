1ceb0db82732ac38cc0d111a4760c80b0ca10f4f
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-01-28 12:52:44 +0530

    5th commit - creating user page, github page. github page inserting into header, fetching followers from github and displaying in github page, changing url of github in footer

--- 07_React-Routers/src/main.jsx
+++ 07_React-Routers/src/main.jsx
@@ -1,32 +1,50 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Home from './components/Home/home.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { path: '',
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
-      }
-    ]
-  }
-])
+// //1st Method
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       { path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+// Same but different method of nesting
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
