0fc4350a240c1629ef2d28eda1522b518013f80f
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-02-19 13:04:54 +0530

    creating Home.jsx and integrating isActive in Header.jsx file

--- 07_React-Routers-Prectice-Project/src/App.jsx
+++ 07_React-Routers-Prectice-Project/src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
 export default function App() {
   return (
-    <h1 className="bg-green-600 p-4">
-      Hello world!
-    </h1>
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
   )
 }
