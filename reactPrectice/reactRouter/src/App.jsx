9737f6429734a4f8f4bd2ec06d93ef1c3277f134
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-04-01 18:22:27 +0530

    precticing of react router

---
+++ reactPrectice/reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
+export default function App() {
+  return (
+    <div>
+    <h1 className="bg-green-600 p-4 text-3xl text-white">Arbaz</h1>
+    <Header/>
+    <Footer/>
+    </div>
+  )
+}
