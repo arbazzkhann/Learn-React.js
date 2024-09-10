83ab86935e83ce579c88d7c9285c2a7a06c2b357
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-10 13:24:59 +0530

    precticing of useRef with example

--- CWH/3_useRef_Hook/src/App.jsx
+++ CWH/3_useRef_Hook/src/App.jsx
@@ -1,26 +1,56 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
+// import { useEffect, useRef, useState } from 'react'
+// import './App.css'
 
-function App() {
-  const [myData, setMyData] = useState("");
-  // const [count, setCount] = useState();
-  const count = useRef(0);
+// function App() {
+//   const [myData, setMyData] = useState("");
+//   // const [count, setCount] = useState();
+//   const count = useRef(0);
+
+//   useEffect(() => {
+//       count.current = count.current + 1;
+//   })
+
+
+//   return (
+//     <>
+//       <input 
+//         type="text"
+//         value={myData.current}
+//         onChange={(e) => setMyData(e.target.value)}
+//       />
+//       <h2>Counter Value: {count.current}</h2>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import React from 'react'
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+  let a = useRef(0);
+  let myFirstInput = useRef()
 
   useEffect(() => {
-      count.current = count.current + 1;
+    myFirstInput.current.style.backgroundColor = "red"
+    a.current = a.current + 1;
+    console.log(`The value of a is: ${a.current}`)
   })
-
 
   return (
     <>
-      <input 
-        type="text"
-        value={myData.current}
-        onChange={(e) => setMyData(e.target.value)}
-      />
-      <h2>Counter Value: {count.current}</h2>
+      <input ref={myFirstInput} type="text" value={count}/><br />
+      <button onClick={() => setCount(count+1)}>Click me</button>
     </>
   )
 }
 
 export default App
+
