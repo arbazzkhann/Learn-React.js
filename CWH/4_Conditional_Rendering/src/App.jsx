75d0fe2b08394051c4ce0d6409b504ec118266ec
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-11 13:04:20 +0530

    installing tailwind css and precticing conditional redering and props

--- CWH/4_Conditional_Rendering/src/App.jsx
+++ CWH/4_Conditional_Rendering/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [showBtn, setShowBtn] = useState(false)
+  const [count, setCount] = useState(0);
+  const [showBtn, setShowBtn] = useState(false);
+
+  const [todo, setTodo] = useState([
+    {
+      name: "Arbaz Khan",
+      id: 2106700884
+    },
+    {
+      name: "Ankit",
+      id: 2106272665
+    }
+  ])
+
+  const Todo = ({todo}) => {
+    return (<>
+      <div className="todo">{todo.name}</div>
+      <div className="todo">{todo.id}</div>
+    </>)
+  }
 
   return (
     <>
@@ -18,25 +36,29 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {showBtn && <button>I will be shown only when second button is clicked</button>}
+      {showBtn && (
+        <button>I will be shown only when second button is clicked</button>
+      )}
       <div className="card">
-        <button onClick={() => {
-              setCount(count + 1)
-              setShowBtn(!showBtn)
-            }
-          }>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            setShowBtn(!showBtn);
+          }}
+        >
           count is {count}
         </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+       <p>
+          {todo.map(todo => {
+            return <Todo todo={todo}/>
+          })}
+       </p>
+
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
