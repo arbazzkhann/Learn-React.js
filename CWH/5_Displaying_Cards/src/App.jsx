ff7135bc8b9f52ae32b70fe4f59f3757df6906af
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-14 11:56:07 +0530

    creating Navbar component and precticing of displaying cars with 'fetch'

--- CWH/5_Displaying_Cards/src/App.jsx
+++ CWH/5_Displaying_Cards/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 //API LINK
 // https://jsonplaceholder.typicode.com/posts 
@@ -13,38 +14,30 @@ function App() {
   const [cards, setCards] = useState([])
 
   const fetchData = async () => {
-    // let a = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let a = await fetch('https://jsonplaceholder.typicode.com/comments');
-    let data = await a.json()
+    let a = await fetch('https://jsonplaceholder.typicode.com/posts ');
+    let data = await a.json();
     setCards(data);
-    console.log(data)
-
+    console.log(data);
   }
 
   useEffect(() => {
     fetchData()
   }, [])
 
+
   return (
     <>
+      <Navbar />
       <div className="container">
         {cards.map((card) => {
-          return (
-            <div key={card.id} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {card.name}
-                </h5>
-              
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {card.email}
-              </p>
-            </div>
-          );
-        }
-      )
-    }
-      </div>      
+          return <div className='card'>
+                    <h1>{card.title}</h1>
+                    <p>{card.body}</p>
+                    <span>by: UserID: {card.id}</span>
+                </div>
+        })}
+      </div>
+
     </>
   )
 }
