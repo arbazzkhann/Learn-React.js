7506bb7b9ecb272bfb869e60d45894c12023e29c
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-14 20:58:48 +0530

    creating jsx testing navbar

---
+++ CWH/7_Making_ToDo_List/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="bg-red-600">Hey I am Red</div>
+    </div>
+  );
+};
+
+export default Navbar;
