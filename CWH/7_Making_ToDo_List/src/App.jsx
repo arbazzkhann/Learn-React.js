4210a0de8956506c746895089ce8be54fbc91974
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-17 13:43:55 +0530

    first designing button and some inputs and headings with tailwind

--- CWH/7_Making_ToDo_List/src/App.jsx
+++ CWH/7_Making_ToDo_List/src/App.jsx
@@ -7,6 +7,26 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]"> 
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" />
+          <button className='text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6'>Add</button>
+        </div>
+          <h2 className='text-lg font-bold'>Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div className="button">
+              <button className='text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6'>Edit</button>
+              <button className='text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6'>Delete</button>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
     </>
   )
 }
--- CWH/7_Making_ToDo_List/src/Components/Navbar.jsx
+++ CWH/7_Making_ToDo_List/src/Components/Navbar.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="bg-red-600">Hey I am Red</div>
-    </div>
+    <nav className="flex justify-between bg-slate-700 text-white py-2 ">
+      <div className="logo">
+        <span className="font-bold text-xl mx-10">iTasks</span>
+      </div>
+      <ul className="flex gap-8 mx-10">  
+        <li className="cursor-pointer hover:font-bold transition-all">Home</li>
+        <li className="cursor-pointer hover:font-bold transition-all">Your tasks</li>
+      </ul>
+    </nav>
   );
 };
 
