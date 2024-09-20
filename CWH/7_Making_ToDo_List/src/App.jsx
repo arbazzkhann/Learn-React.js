7f3cd0090aa665d6df1b26f819c6cc0a795c0c25
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-20 16:38:01 +0530

    creating editi button working and formatting code

--- CWH/7_Making_ToDo_List/src/App.jsx
+++ CWH/7_Making_ToDo_List/src/App.jsx
@@ -7,12 +7,23 @@ function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const handleEdit = () => {
+  const handleEdit = (e, id) => {
+    let t = todos.filter(item => item.id === id)
+    setTodo(t[0].todo)
 
+    //deleting todo
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
   };
 
   const handleDelete = (e, id) => {
-    console.log(`The id is : ${id}`)
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+
+    setTodos(newTodos)
   };
 
   const handleAdd = () => {
@@ -42,47 +53,31 @@ function App() {
       <Navbar />
 
       <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+
         <div className="addTodo">
           <h2 className="text-lg font-bold">Add a Todo</h2>
-          <input
-            onChange={handleChange}
-            value={todo}
-            type="text"
-            className="w-1/2"
-          />
-          <button
-            onClick={handleAdd}
-            className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6"
-          >
-            Add
-          </button>
+          <input onChange={handleChange} value={todo} type="text" className="w-1/2"/>
+          <button onClick={handleAdd} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6">Save</button>
         </div>
 
         <h2 className="text-lg font-bold">Your Todos</h2>
 
         <div className="todos">
+          {todos.length === 0 && <div className="m-5">No todos to display</div>}
           {todos.map((item) => {
             return (
-              <div
-                key={item.todo}
-                className="todo flex w-2/3 my-3 justify-between"
-              >
-                <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} id="" />
-                <div className={item.isCompleted? "line-through": ""}>{item.todo}</div>
-                <div className="button">
-                  <button
-                    onClick={handleEdit}
-                    className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={(e)=> {handleDelete(e, item.id)}}
-                    className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1"
-                  >
-                    Delete
-                  </button>
+
+              <div key={item.todo} className="todo flex w-2/3 my-3 justify-between">
+                <div className="flex gap-5">
+                  <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} id="" />
+                  <div className={item.isCompleted? "line-through": ""}>{item.todo}</div>
                 </div>
+
+                <div className="button">
+                  <button onClick={(e) => handleEdit(e, item.id)} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1">Edit</button>
+                  <button onClick={(e) => {handleDelete(e, item.id)}} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1">Delete</button>
+                </div>
+
               </div>
             );
           })}
