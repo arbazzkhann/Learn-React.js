5014deb9c912273f3bc4766d32e287eb76c3654e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-09-21 12:48:15 +0530

    completing all functionality of todo list app in react

--- CWH/7_Making_ToDo_List/src/App.jsx
+++ CWH/7_Making_ToDo_List/src/App.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar.jsx";
 import { v4 as uuidv4 } from 'uuid';
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 function App() {
 
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [showFinished, setShowFinished] = useState(false)
 
   const saveToLocalStorage = (params) => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -19,6 +23,14 @@ function App() {
       setTodos(todos)
     }
   }, [])
+
+// handling show finished checkbox
+  const toggleFinished = (e) => {
+    setShowFinished(!showFinished)
+
+    //saving to local storage
+    saveToLocalStorage();
+  }
 
 
   const handleEdit = (e, id) => {
@@ -75,30 +87,37 @@ function App() {
     <>
       <Navbar />
 
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
 
-        <div className="addTodo">
+      <h1 className="font-bold text-center text-2xl">iTasks - Manage your todos at one place</h1>
+
+        <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className="text-lg font-bold">Add a Todo</h2>
-          <input onChange={handleChange} value={todo} type="text" className="w-1/2"/>
-          <button onClick={handleAdd} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-6">Save</button>
+
+          <div className="flex">
+            <input onChange={handleChange} value={todo} type="text" className="w-full rounded-full px-5 py-1 "/>
+            <button onClick={handleAdd} disabled={todo.length < 3} className="disabled:bg-violet-200  text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 mx-4 px-5 rounded-full">Save</button>
+          </div>
         </div>
 
-        <h2 className="text-lg font-bold">Your Todos</h2>
+        <input onChange={toggleFinished} type="checkbox" checked={showFinished}/> Show finished
+
+        <h2 className="my-4 text-lg font-bold">Your Todos</h2>
 
         <div className="todos">
           {todos.length === 0 && <div className="m-5">No todos to display</div>}
           {todos.map((item) => {
-            return (
+            return (showFinished || !item.isCompleted) && (
 
-              <div key={item.todo} className="todo flex w-2/3 my-3 justify-between">
+              <div key={item.todo} className="todo flex justify-between">
                 <div className="flex gap-5">
-                  <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} id="" />
+                  <input name={item.id} type="checkbox" onChange={handleCheckbox} checked={item.isCompleted} />
                   <div className={item.isCompleted? "line-through": ""}>{item.todo}</div>
                 </div>
 
-                <div className="button">
-                  <button onClick={(e) => handleEdit(e, item.id)} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1">Edit</button>
-                  <button onClick={(e) => {handleDelete(e, item.id)}} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1">Delete</button>
+                <div className="button flex justify-center items-center h-full">
+                  <button onClick={(e) => handleEdit(e, item.id)} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1"><FaEdit /></button>
+                  <button onClick={(e) => {handleDelete(e, item.id)}} className="text-white bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 rounded-lg mx-1"><MdDelete /></button>
                 </div>
 
               </div>
