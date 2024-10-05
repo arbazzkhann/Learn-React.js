83f7d8c8e12da088505916810d29fc63a97ee454
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-05 19:52:39 +0530

    some changes in validation

--- CWH/10_Handling_Forms/src/App.jsx
+++ CWH/10_Handling_Forms/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <>
       <div className="container">
         <form action="" onSubmit={handleSubmit(onSubmit)}>
-          <input type='text' {...register("username", {required: {value: true, message:"Username is required."}, minLength: {value: 3, message: "Farhan Minimum length is 3"}, maxLength: {value: 50, message: "Farhan Maximum length is 50"}})} placeholder='username'/>
+          <input type='text' {...register("username", {required: {value: true, message:"Username is required."}, minLength: {value: 3, message: "Minimum length is 3"}, maxLength: {value: 12, message: "Maximum length is 12"}})} placeholder='username'/>
           {errors.username && <div className='red'>{errors.username.message}</div>}
           <br />
           <input type='password' {...register("password")} placeholder="password"/>
