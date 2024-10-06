a12d35ee6f10d011a7945b529eb6eb26b3a8617f
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-06 12:42:43 +0530

    putting more validation in password and some errors

--- CWH/10_Handling_Forms/src/App.jsx
+++ CWH/10_Handling_Forms/src/App.jsx
@@ -9,24 +9,46 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    setError,
+    formState: { errors, isSubmitting },
   } = useForm()
 
-  const onSubmit = (data) => console.log(data)
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve() 
+      }, d*1000)
+    })
+  }
+
+  const onSubmit = async (data) => {
+    await delay(2)
+    console.log(data)
+    if(data.username == "aman") {
+      setError("amanError", {message: "Sorry this user is blocked."});
+    }
+    if(data.password == data.username) {
+      setError("chooseDifferentPassword", {message: "Username and password must be different."})
+    }
+  }
 
   return (
     <>
+      {isSubmitting && <div>Loading...</div>}
       <div className="container">
         <form action="" onSubmit={handleSubmit(onSubmit)}>
           <input type='text' {...register("username", {required: {value: true, message:"Username is required."}, minLength: {value: 3, message: "Minimum length is 3"}, maxLength: {value: 12, message: "Maximum length is 12"}})} placeholder='username'/>
           {errors.username && <div className='red'>{errors.username.message}</div>}
           <br />
-          <input type='password' {...register("password")} placeholder="password"/>
+          <input type='password' {...register("password", {required: {value: true, message: "Password is required."}, minLength: {value: 3, message: "Minimum length is 6"}})} placeholder="password"/>
+          {errors.password && <div className='red'>{errors.password.message}</div>}
           <br />
-          <input type="submit" value="submit" />
+          <input disabled={isSubmitting} type="submit" value="submit" />
+          {errors.amanError && <div className='red'>{errors.amanError.message}</div>}
+          {errors.chooseDifferentPassword && <div className='red'>{errors.chooseDifferentPassword.message}</div>}
         </form>
       </div>
-    </>
+    </> 
   )
 }
 
