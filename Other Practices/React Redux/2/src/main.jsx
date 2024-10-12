2ebc7151f13594246bf029875f0237c54d0f8982
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-12 18:33:47 +0530

    precticing core react-redux and redux (createStore, reducer, Provider, useDispatch, useSelector etc)

--- Other Practices/React Redux/2/src/main.jsx
+++ Other Practices/React Redux/2/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Provider } from 'react-redux'
+import { Store } from './Redux/Store.js'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    </StrictMode>,
+
 )
