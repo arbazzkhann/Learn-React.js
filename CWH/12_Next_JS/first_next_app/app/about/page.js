a7a415dcfba42648fa759d21a43be1130bf8d8bf
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-10-14 12:04:22 +0530

    final creating first next js app

---
+++ CWH/12_Next_JS/first_next_app/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold m-4">I am about page.</h1>
+        <ui className="flex flex-col ml-4">
+            <li>Full stack solution</li>
+            <li>Provides file based routing</li>
+            <li>Additional features like router from next/navigation</li>
+            <li>Optimized rendering</li>
+            <li>SEO friendly</li>
+        </ui>
+    </div>
+  )
+}
+
+export default page
