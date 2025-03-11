a9a2cfb45326791b66f61403333c0d169e5bb20e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-11 19:53:29 +0530

    changing increase count function to inline function in react

--- 00Prectice Folder/vite-project/src/App.jsx
+++ 00Prectice Folder/vite-project/src/App.jsx
@@ -6,11 +6,7 @@ function App() {
 
   function increaseCount() {
 
-      if(count >= 0 && count < 10) {
-        count++;
-        document.querySelector(".myH2").innerText = count;
-  
-      }
+      
   }
   function decreaseCount() {
     if(count > 0) {
@@ -25,7 +21,12 @@ function App() {
       <div>
           
           <h2 className="myH2 text-4xl font-bold dark:text-white">0</h2>
-          <button onClick={increaseCount} type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Increase count</button>
+          <button onClick={() => {
+                if(count >= 0 && count < 10) {
+                  count++;
+                  document.querySelector(".myH2").innerText = count;
+                }}} 
+                type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Increase count</button>
           <button onClick={decreaseCount} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Decrease count</button>
       </div>
     </>
