71206be2af0a936c1bfc17fe48dacd0a5c4ddc8d
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-11 17:31:18 +0530

    vite app cards and props and destructuring

--- 00Prectice Folder/vite-project/src/App.jsx
+++ 00Prectice Folder/vite-project/src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
 import Card from './Components/Card'
 import React from 'react'
 
 
 function App() {
-  let [count, setCount] = useState(0)
-
-  function increaseCount() {
-    count++;
-    setCount(count);
-  }
 
   return (
     <>
-      <Card count={count}/>
-      <h1>Count: {count} (from App.jsx)</h1>
-      <button onMouseOver={increaseCount}>Click me!</button>
-
+      <Card name={"React"} desc={"Creating single page applications."}/>
+      <Card name={"Tailwind CSS"} desc={"CSS Library."}/>
     </>
   )
 }
--- 00Prectice Folder/vite-project/src/Components/Card.jsx
+++ 00Prectice Folder/vite-project/src/Components/Card.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-function Card({count}) {
-    // let [count] = prop;
-    return (
-        <div>
-            <h1>Count: {count} (from external component)</h1>
-        </div>
-    )
+function Card({ name , desc}) {
+  // let [count] = prop;
+  return (
+    <a
+      href="#"
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {name}
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400">
+        {desc}
+      </p>
+    </a>
+  );
 }
 
 export default Card;
