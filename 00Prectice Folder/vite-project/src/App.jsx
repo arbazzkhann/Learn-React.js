7a7d37edce10becf67dab8ca145a62a8385391a6
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-12 11:57:30 +0530

    useEffect hook

--- 00Prectice Folder/vite-project/src/App.jsx
+++ 00Prectice Folder/vite-project/src/App.jsx
@@ -1,33 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
 
-  let count = 0;
+  let [count, setCount] = useState(0);
+  let [data, setData] = useState(0);
 
-  function increaseCount() {
+  useEffect(() => {
+    console.log("Use Effect run", data);
+  }, [data])
 
-      
-  }
-  function decreaseCount() {
-    if(count > 0) {
-      count--;
-      document.querySelector(".myH2").innerText = count;
-    }
 
-  }
 
   return (
     <>
       <div>
-          
-          <h2 className="myH2 text-4xl font-bold dark:text-white">0</h2>
-          <button onClick={() => {
-                if(count >= 0 && count < 10) {
-                  count++;
-                  document.querySelector(".myH2").innerText = count;
-                }}} 
-                type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Increase count</button>
-          <button onClick={decreaseCount} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Decrease count</button>
+        <h1>{count}</h1>
+        <button className="bg-red-600" onClick={() => {count++, setCount(count)}}>Count</button>
+        <h2>{data}</h2>
+        <button className="bg-green-600" onClick={() => {data++, setData(data)}}>Data</button>
       </div>
     </>
   )
