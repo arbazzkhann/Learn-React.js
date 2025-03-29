421b60407b3643a50f28a6053fbc2416cdac0196
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-29 16:28:22 +0530

    increasing count and decreasing count function and displaying count state on Card component

---
+++ 00Prectice Folder/react-prectice/src/Components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Card({count}) {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <h3>{count}</h3>
+        </>
+    )
+}
+
+
+// import React from 'react'
+
+// const Card = ({count}) => {
+//   return (
+//     <>
+//         <h1>Hello world!</h1>
+//         <h3>{count}</h3>
+//     </>
+//   )
+// }
+
+// export default Card
