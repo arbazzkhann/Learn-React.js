224f40f29d39628a0b895dfa252d1246b1789ac9
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-09 17:58:38 +0530

    updating card with tailwind css

--- 00Prectice Folder/react-prectice/src/App.jsx
+++ 00Prectice Folder/react-prectice/src/App.jsx
@@ -10,15 +10,18 @@ function App() {
 
   let myArr = [{
     name: "Arbaz Khan",
-    age: 22
+    age: 22,
+    image: "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg"
     },
     {
       name: "Mohd. Farhan",
-      age: 9
+      age: 9,
+      image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg"
     },
     {
       name: "MD. Subhan",
-      age: 16
+      age: 16,
+      image: "https://gratisography.com/wp-content/uploads/2023/06/gratisography-happy-food-free-stock-photo-1170x780.jpg"
     }
   ] 
 
@@ -27,7 +30,7 @@ function App() {
       <div>
         {
           myArr.map((item) => {
-            return <InfoCard name={item.name} age={item.age}/>
+            return <InfoCard name={item.name} age={item.age} image={item.image}/>
           })
         }
       </div>
--- 00Prectice Folder/react-prectice/src/Components/InfoCard.jsx
+++ 00Prectice Folder/react-prectice/src/Components/InfoCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function InfoCard(props) {
-    return (
-        <>
-            <div>
-                <h2>i am Info Card</h2>
-                <p>Hello, {props.name} </p>
-                <p>Age: {props.age}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-64 h-40" src={props.image} alt="Sunset in the mountains" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{props.name}</div>
+          <p class="text-gray-700 text-base">{props.age}</p>
+        </div>
+        <div class="px-6 pt-4 pb-2"></div>
+      </div>
+      Horizontal
+    </>
+  );
 }
