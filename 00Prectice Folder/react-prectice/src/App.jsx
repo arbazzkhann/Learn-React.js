7502015d445c0ca016e95913b04943b41d14027a
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-10 12:15:52 +0530

    creating Resorces/JSON_DATA directories and creating InfoCard.js and putting infoCard details array, seprating infoCard details array form App.jsx

--- 00Prectice Folder/react-prectice/src/App.jsx
+++ 00Prectice Folder/react-prectice/src/App.jsx
@@ -4,26 +4,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import InfoCard from './Components/InfoCard'
+import myArr from './Resources/JSON_DATA/InfoCard'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  let myArr = [{
-    name: "Arbaz Khan",
-    age: 22,
-    image: "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg"
-    },
-    {
-      name: "Mohd. Farhan",
-      age: 9,
-      image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg"
-    },
-    {
-      name: "MD. Subhan",
-      age: 16,
-      image: "https://gratisography.com/wp-content/uploads/2023/06/gratisography-happy-food-free-stock-photo-1170x780.jpg"
-    }
-  ] 
 
   return (
     <>
---
+++ 00Prectice Folder/react-prectice/src/Resources/JSON_DATA/InfoCard.js
@@ -0,0 +1,21 @@
+let myArr = [
+  {
+    name: "Arbaz Khan",
+    age: 22,
+    image: "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg",
+  },
+  {
+    name: "Mohd. Farhan",
+    age: 9,
+    image:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/new-year-background-736885_1280.jpg",
+  },
+  {
+    name: "MD. Subhan",
+    age: 16,
+    image:
+      "https://gratisography.com/wp-content/uploads/2023/06/gratisography-happy-food-free-stock-photo-1170x780.jpg",
+  },
+];
+
+export default myArr;
