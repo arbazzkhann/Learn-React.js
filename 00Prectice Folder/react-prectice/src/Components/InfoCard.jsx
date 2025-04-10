287ee82ac3474fa635819bddb3a40b2e7c30123d
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-10 12:18:40 +0530

    chaning class to className

--- 00Prectice Folder/react-prectice/src/Components/InfoCard.jsx
+++ 00Prectice Folder/react-prectice/src/Components/InfoCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function InfoCard(props) {
   return (
     <>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-64 h-40" src={props.image} alt="Sunset in the mountains" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{props.name}</div>
-          <p class="text-gray-700 text-base">{props.age}</p>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-64 h-40" src={props.image} alt="Sunset in the mountains" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{props.name}</div>
+          <p className="text-gray-700 text-base">{props.age}</p>
         </div>
-        <div class="px-6 pt-4 pb-2"></div>
+        <div className="px-6 pt-4 pb-2"></div>
       </div>
       Horizontal
     </>
