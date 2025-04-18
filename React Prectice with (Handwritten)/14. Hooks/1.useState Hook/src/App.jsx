d3a27407fb7e531d1e189879c45e2c5878ec63d3
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-18 11:57:55 +0530

    useState prectice

--- React Prectice with (Handwritten)/14. Hooks/1.useState Hook/src/App.jsx
+++ React Prectice with (Handwritten)/14. Hooks/1.useState Hook/src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); //useSTate hook
 
   return (
     <>
       <div>
-        
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
       </div>
     </>
   )
