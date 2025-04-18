b5eee87938d8ca9e7c669e34bbdb1957844cd892
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-18 16:38:53 +0530

    creating vite-app for useContext prectice

---
+++ React Prectice with (Handwritten)/14. Hooks/4. useContext/src/App.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+   
+      </div>
+    </>
+  )
+}
+
+export default App
