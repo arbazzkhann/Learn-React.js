d06ef57d28b5ddf5a0eac396dc17e77a4f0405ca
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-23 21:12:40 +0530

    useReducer final prectice for counter

--- React Prectice with (Handwritten)/14. Hooks/7. useReducer/src/App.jsx
+++ React Prectice with (Handwritten)/14. Hooks/7. useReducer/src/App.jsx
@@ -1,25 +1,63 @@
-import { useReducer, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'Increment': return {count: state.count + 1};
-      default: return state;
+  //code 1
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case 'Increment': return {count: state.count + 1};
+  //     default: return state;
+  //   }
+  // };
+
+  // const [state, dispatch] = useReducer(reducer, {count: 0});
+
+  //code 2
+  // --------------------------------------------------------------
+  /*
+  Syntax:
+    const [state, dispatch] = useReducer(reducerFunction, initialState);
+
+    reducerFunction(currentState, action) {
+    }
+  */
+  const initialState = 0;
+  const reducerFunction = (currentState, action) => {
+    switch (action) {
+      case "increment":
+        return currentState + 1;
+      case "decrement":
+        return currentState - 1;
+      default:
+        return currentState;
     }
   };
 
-  const [state, dispatch] = useReducer(reducer, {count: 0});
+  function Counter() {
+    const [count, dispatch] = useReducer(reducerFunction, initialState);
+
+    return (
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch("increment")}>Increment</button>
+        <button onClick={() => dispatch("decrement")}>Decrement</button>
+      </div>
+    );
+  }
 
   return (
     <>
       <div>
-        <button onClick={() => dispatch({type: "Increment"})}>Increment {state.count}</button>
+        {/* //code 1 */}
+        {/* <button onClick={() => dispatch({type: "Increment"})}>Increment {state}</button> */}
+
+        {/* //code 2 */}
+        <Counter />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
