8d91113de6472c9c5c1464ba1f45c6cafbddbc95
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-24 19:08:13 +0530

    useParams in react-router

--- React Prectice with (Handwritten)/17. Routers/Dynamic Routes (with Params)/src/App.jsx
+++ React Prectice with (Handwritten)/17. Routers/Dynamic Routes (with Params)/src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider, useParams } from 'react-router-dom';
+
+import Home from './Components/Home'
+import About from './Components/About'
+
+function UserProfile() {
+  const {id} = useParams();
+  return <h2>User ID: {id}</h2>
+}
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/user/:id",
+      element: <UserProfile />
+    }
+  ])
 
   return (
     <>
       <div>
-        
+        <RouterProvider router={router}/>
       </div>
     </>
   )
