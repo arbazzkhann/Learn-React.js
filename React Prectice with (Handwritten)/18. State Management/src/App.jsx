614e78f68b1018862e7931d8d6e54f1862a122bf
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-25 19:52:07 +0530

    instaling tailwind css and creating designing button for count like increase, decrease and reset with useContext API

--- React Prectice with (Handwritten)/18. State Management/src/App.jsx
+++ React Prectice with (Handwritten)/18. State Management/src/App.jsx
@@ -1,22 +1,152 @@
-import React, { useState } from 'react'
-import myCountContext from './Components/Context'
-import Navbar from './Components/Navbar';
-import Farhan from './Components/Farhan';
-
+import React, { useState } from "react";
+import myCountContext from "./Components/Context";
+import Navbar from "./Components/Navbar";
+import Farhan from "./Components/Farhan";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <myCountContext.Provider value={{count, setCount}}>
+    <myCountContext.Provider value={{ count, setCount }}>
       <div>
         <Navbar />
-        <button onClick={() => setCount(count + 1)}>Click me!</button>
 
-        <Farhan/>
+{/* Increase Button */}
+        <button
+          onClick={() => setCount(count + 1)}
+          class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+        >
+          <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+          <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+            Increase me!
+          </span>
+        </button>
+
+
+{/* Decrease Button */}
+        <button
+          onClick={() => setCount(count - 1)}
+          class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+        >
+          <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+          <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+            Decrease me!
+          </span>
+        </button>
+
+<br />
+
+{/* Reset Button */}
+        <button
+          onClick={() => setCount(0)}
+          class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+        >
+          <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+          <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg
+              class="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+            Reset!
+          </span>
+        </button>
+
+        <Farhan />
       </div>
     </myCountContext.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
