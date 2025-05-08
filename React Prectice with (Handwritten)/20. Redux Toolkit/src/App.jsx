c96c86ae746fd29347945d6c7a9719dc4a1ac6f5
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-08 19:25:25 +0530

    vite-react starter code for learning redux-toolkit

--- React Prectice with (Handwritten)/20. Redux Toolkit/src/App.jsx
+++ React Prectice with (Handwritten)/20. Redux Toolkit/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("white");
 
   return (
     <>
       <div>
-        <input type="text" value={color}/>
-        <button onClick={() => setColor("Red")}>Red</button>
-        <button onClick={() => setColor("Green")}>Green</button>
+        
       </div>
     </>
   )
