8c610341567b47e53b7cd7ed08a08d1c0654fee9
arbazzkhann <arbazkha78az@gmail.com>
2025-05-22 20:31:24 +0530

    maxLength

--- 00Prectice Folder/react-prectice/React-Hook-Form/src/App.jsx
+++ 00Prectice Folder/react-prectice/React-Hook-Form/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
             {...register("firstName", {
               required: true,
               minLength: { value: 3, message: "Minimum length atleast 3" },
+              maxLength: { value: 10, message: "Maximum length upto 10" },
             })}
           />
           {errors.firstName && <p>{errors.firstName.message}</p>}
