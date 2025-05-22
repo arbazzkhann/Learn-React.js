da794777e87e82cfd7fab7e9c97b2fbf8be8231b
arbazzkhann <arbazkha78az@gmail.com>
2025-05-22 20:39:17 +0530

    applying classes o errors

--- 00Prectice Folder/react-prectice/React-Hook-Form/src/App.jsx
+++ 00Prectice Folder/react-prectice/React-Hook-Form/src/App.jsx
@@ -20,13 +20,14 @@ function App() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <label>First Name: </label>
           <input
+            
             {...register("firstName", {
               required: true,
               minLength: { value: 3, message: "Minimum length atleast 3" },
               maxLength: { value: 10, message: "Maximum length upto 10" },
             })}
           />
-          {errors.firstName && <p>{errors.firstName.message}</p>}
+          {errors.firstName && <p className={errors.firstName ? "red error-border" : ""}>{errors.firstName.message}</p>}
 
           <label>Last Name: </label>
           <input {...register("lastName")} />
