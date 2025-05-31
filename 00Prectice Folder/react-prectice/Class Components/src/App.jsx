165a313ea6afa1ef6c51e1ed4124cf14980dc552
Arbaz Khan <arbazkha78az@gmail.com>
2025-05-31 17:09:14 +0530

    class components in react

---
+++ 00Prectice Folder/react-prectice/Class Components/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ClassComponent from './Components/ClassComponent'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ClassComponent />
+      </div>
+    </>
+  )
+}
+
+export default App
