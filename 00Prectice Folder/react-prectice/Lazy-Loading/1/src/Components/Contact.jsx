cd1037411c58c6cfca77d27b8fe8ef9ef0f5dfd3
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-02 12:12:32 +0530

    creating Navbar with also using Link, NavLink, creating router based navbar - About, contact, home page route create

---
+++ 00Prectice Folder/react-prectice/Lazy-Loading/1/src/Components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from './Navbar'
+
+const Contact = () => {
+  return (
+    <div>
+        <Navbar />
+        <h1>I am Contact.</h1>
+    </div>
+  )
+}
+
+export default Contact
