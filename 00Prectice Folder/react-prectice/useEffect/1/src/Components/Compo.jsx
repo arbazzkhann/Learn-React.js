e8da9c33b23921b28bed0c79239e37d7c4638aac
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-07 09:59:39 +0530

    useEffect - with empty dependency array and their description

--- 00Prectice Folder/react-prectice/useEffect/1/src/Components/Compo.jsx
+++ 00Prectice Folder/react-prectice/useEffect/1/src/Components/Compo.jsx
@@ -6,14 +6,24 @@ const Compo = () => {
     const [compoCount, setCompoCount] = useState(0);
     
     //With dependency array
+    //Code will executes when dependency array element changes.
     // useEffect(() => {
     //     console.log("Hello world");
     // }, [compoCount]);
 
-    //Empty dependency array
+    //without dependency array:
+    //code will executes when any state changes.
+    // useEffect(() => {
+    //     console.log("Hello world");
+    // });
+
+    //Empty dependency array:
+    //Nothing changed when dependency array is empty.
     useEffect(() => {
         console.log("Hello world");
-    })
+    }, []);
+
+
 
   return (
     <div>
